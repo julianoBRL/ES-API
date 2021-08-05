228166b843a2af8984d2e40b505cfbcb014c46fe
Shintaro-PC <shintaro0dev@gmail.com>
2021-08-05 00:02:05 -0300

    Genre Singletron Cache

---
+++ src/main/java/com/shintaro/sysmulapi/entitys/genre/GenreSingletron.java
@@ -0,0 +1,8 @@
+package com.shintaro.sysmulapi.entitys.genre;
+
+import org.springframework.stereotype.Service;
+
+import com.shintaro.sysmulapi.generic.Singletron;
+
+@Service
+public class GenreSingletron extends Singletron<GenreEntity> {}
